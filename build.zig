e1cc788ebe743744938b298ecc97edb141fa7444
Łukasz Wala <lukaszpwala@gmail.com>
2024-06-27 21:04:15 +0200

    Move executable files to `example`, change `build.zig` to treat the project like a library

--- build.zig
+++ build.zig
@@ -4,42 +4,18 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const xev = b.dependency("libxev", .{ .target = target, .optimize = optimize });
+    _ = b.addModule("zaft", .{
+        .root_source_file = b.path("src/root.zig"),
+    });
 
-    const exe = b.addExecutable(.{
-        .name = "zaft",
-        .root_source_file = b.path("src/main.zig"),
+    const test_exe = b.addTest(.{
+        .name = "zaft-test",
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    exe.root_module.addImport("xev", xev.module("xev"));
-
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    const test_run = b.addRunArtifact(test_exe);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&test_run.step);
 }
